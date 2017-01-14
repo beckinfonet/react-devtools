57e97b59151ba6b43eef18d2881028ece4ccde17
Dan Abramov <dan.abramov@gmail.com>
2017-01-14 13:30:03 +0000

    Use fiber

--- test/example/target.js
+++ test/example/target.js
@@ -12,7 +12,7 @@
 'use strict';
 
 var React = require('react');
-var ReactDOM = require('react-dom');
+var ReactDOM = require('react-dom/fiber');
 
 var node = document.createElement('div');
 document.body.appendChild(node);
